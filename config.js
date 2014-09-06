e0aec7b9ebbd7f472e995549e3963b54d3f59228
gamwang <jonbai23@gmail.com>
2014-09-06 10:22:35 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+var oauthTokenSecret = 'd15970aabcf71e2e'; 
+var oauthToken = "S=s1:U=8f613:E=14fa18711d7:C=14849d5e278:P=1cd:A=en-devtoken:V=2:H=8cb95491515dfd2a0bb42b2a94f2e372";
+var oauthVerifier = "gamwang";
+
+module.exports = {
+    secret: oauthTokenSecret,
+    authToken: oauthToken,
+    id: oauthVerifier
+};
